1a9a8eb39699c3c5f9747b7b2438f428b0a291ff
Rastin Rastgoufard <rastin@unocity.us>
2023-01-07 05:10:43 -0600

    preliminarily working!  UIUX is rough though...

--- morph_lane_linker.lua
+++ morph_lane_linker.lua
@@ -11,12 +11,13 @@ function factory()
   desired_links = {
 
   -- morph lane, plugin id, parameter name, value at 0, value at 1
-    {"p0", 19561, "Frequency 0", 10000, 50},
-    {"p0", 19561, "Gain 0", 0.25, 13.5},
+    {"p0", 35775, "Frequency 0", 10000, 50},
+--     {"p0", 30445, "Gain 0", 0.25, 13.5},
     
   }
 
   all_plugins = {}
+  morph_instances = {}
 
   function collect_plugins()
     print()
@@ -31,9 +32,12 @@ function factory()
         if proc:isnil() then break end
         local pi = proc:to_insert()
         local pp = pi:plugin(0)
-        local name = pi:type() .. "-" .. pp:unique_id()
+        local name = pi:type() .. "-" .. pp:unique_id() .. " named: " .. pp:label()
         local id = pp:id():to_s()
         print("    " .. i .. " " .. id .. " " .. name)
+        if pp:label() == "Morph Processor" then
+          table.insert(morph_instances, proc)
+        end
         all_plugins[id] = proc
         
         i = i + 1
@@ -41,7 +45,16 @@ function factory()
     end
   end
   
+  function configure_morph_processors()
+    print("Configuring morph processors")
+    for i, proc in pairs(morph_instances) do
+      print(i, proc)
+    end
+  end
+  
   function link_lanes()
+  
+    print("Linking lanes")
     
     print(desired_links)
     print(all_plugins)
@@ -59,12 +72,21 @@ function factory()
       
       print(source_lane, plugin_id, target_lane_name)
       
-      plugin = all_plugins[plugin_id]:to_automatable()
-      print(plugin)
-      for p in plugin:all_automatable_params():iter() do
-        print("  ", p)
+      proc = all_plugins[plugin_id]
+      if not proc then
+        print("Uh oh... proc", plugin_id, "does not exist...")
+      else
+        plug = proc:to_insert():plugin(0)
+        print(plug:label())
+        param_count = 0
+        for j=0, plug:parameter_count() - 1 do
+          if plug:parameter_is_control(j) then
+            local label = plug:parameter_label(j)
+            print("    ", param_count, " ", label)
+            param_count = param_count + 1
+          end
+        end
       end
-      
     end
     
     
@@ -79,19 +101,58 @@ function factory()
 
 
   collect_plugins()
+--   configure_morph_processors()
   link_lanes()
+  
+  function get_interp(m, value, start)
+    count = math.floor(ARDOUR.LuaAPI.get_processor_param(m, start))
+    scaled = value * (count - 1)
+    lower_idx = math.floor(scaled)
+    upper_idx = math.ceil(scaled)
+    percentage = scaled - lower_idx
+    lower_value = ARDOUR.LuaAPI.get_processor_param(m, start+lower_idx+1)
+    upper_value = ARDOUR.LuaAPI.get_processor_param(m, start+upper_idx+1)
+    return (1-percentage)*lower_value + percentage*upper_value
+  end
+  
+  function do_the_morph(m)
+    MAX_TARGETS = 4
+    PARAMS_PER_TARGET = 14
+    value = ARDOUR.LuaAPI.get_processor_param(m, 0)
+    for t=0,MAX_TARGETS-1 do
+      local start = t*PARAMS_PER_TARGET + 1
+      local plugin_id = math.floor(ARDOUR.LuaAPI.get_processor_param(m, start+11))
+      local nth_param = math.floor(ARDOUR.LuaAPI.get_processor_param(m, start+12))
+      local enabled = ARDOUR.LuaAPI.get_processor_param(m, start+13) > 0.5
+      local target = all_plugins[tostring(plugin_id)]
+      if enabled then
+        if target then
+          interped = get_interp(m, value, start)
+          ARDOUR.LuaAPI.set_processor_param(target, nth_param, interped)
+        else
+          if plugin_id >= 0 then
+            print("Target with id", plugin_id, "not found?")
+          end
+        end
+      end
+    end
+  end
+  
   
 
   return function(n_samples)
-    local route = Session:route_by_name("Morph")
-    assert (not route:isnil())
-    
-    local plugin = route:nth_plugin(0)
-    assert (not plugin:isnil())
-    
-    -- get the value of p0 and forcibly set p1 and p2 to that value
-    local value = ARDOUR.LuaAPI.get_processor_param(plugin, 0)
-    ARDOUR.LuaAPI.set_processor_param(plugin, 1, value)
-    ARDOUR.LuaAPI.set_processor_param(plugin, 2, value)
+    for _,m in pairs(morph_instances) do
+      do_the_morph(m)
+    end
+--     local route = Session:route_by_name("Morph")
+--     assert (not route:isnil())
+--     
+--     local plugin = route:nth_plugin(0)
+--     assert (not plugin:isnil())
+--     
+--     -- get the value of p0 and forcibly set p1 and p2 to that value
+--     local value = ARDOUR.LuaAPI.get_processor_param(plugin, 0)
+--     ARDOUR.LuaAPI.set_processor_param(plugin, 1, value)
+--     ARDOUR.LuaAPI.set_processor_param(plugin, 2, value)
   end
 end
--- morph_processor.lua
+++ morph_processor.lua
@@ -7,6 +7,8 @@ ardour {
   description = [[generalized Morph for controlling multiple automation lanes.  This processor contains multiple lanes and needs to be coupled with the Session Script morph_lane_linker.lua]]
 }
 
+MAX_TARGETS = 4
+
 function dsp_ioconfig()
   return {
     {audio_in = -1, audio_out = -1},
@@ -14,16 +16,28 @@ function dsp_ioconfig()
 end
 
 function dsp_params()
-  return {
-    { ["type"] = "input", name = "p0", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p1", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p2", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p3", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p4", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p5", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p6", min = 0, max = 1, default = 0 },
-    { ["type"] = "input", name = "p7", min = 0, max = 1, default = 0 },
+  local output = {
+    { ["type"] = "input", name = "controller", min = 0, max = 1, default = 0 },
   }
+  
+  for i=0, MAX_TARGETS-1 do
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control_point_count", min = 2, max = 10, default = 2, integer = true })  -- how many points to use for control.  0 means disabled
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control0", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control1", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control2", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control3", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control4", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control5", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control6", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control7", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control8", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "target" .. i .. "_control9", min = -99999, max = 99999, default = 0 })
+    table.insert(output, { ["type"] = "input", name = "__target" .. i .. "_plugin_id", min = -1, max = 9999999, default = -1, integer = true })
+    table.insert(output, { ["type"] = "input", name = "__target" .. i .. "_nth_param", min = -1, max = 9999999, default = -1, integer = true })
+    table.insert(output, { ["type"] = "input", name = "__target" .. i .. "_enabled", min = 0, max = 1, default = 0, integer = true })
+  end
+  
+  return output
 end
 
 local sample_rate = 0
